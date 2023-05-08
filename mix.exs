a5b1f1061acc30dc566ad15f1736ddb08bab7269
Aleksandar Dimov <isavitaisa@gmail.com>
2023-05-08 23:13:13 +0100

    Add ex_doc as only: :dev dependency

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule WiseGPTEx.MixProject do
   def project do
     [
       app: :wise_gpt_ex,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -31,7 +31,8 @@ defmodule WiseGPTEx.MixProject do
     [
       {:jason, "~> 1.0"},
       {:httpoison, "~> 1.8"},
-      {:mox, "~> 1.0", only: :test}
+      {:mox, "~> 1.0", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
