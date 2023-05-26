b9eb88b644fb8004997c803829823cb6faed9ce3
Aleksandar Dimov <isavitaisa@gmail.com>
2023-05-26 15:23:05 +0100

    Add specs to OpenAIUtils functions

--- lib/wise_gpt_ex/openai_utils.ex
+++ lib/wise_gpt_ex/openai_utils.ex
@@ -11,6 +11,7 @@ defmodule WiseGPTEx.OpenAIUtils do
       iex> WiseGPTEx.OpenAIUtils.reasoning_prompt("What is 2+2?")
       "Question: What is 2+2?\nAnswer: Let's work this out in a step by step way to be sure we have the right answer."
   """
+  @spec reasoning_prompt(binary()) :: binary()
   def reasoning_prompt(message) do
     ~s(Question: #{message}\nAnswer: Let's work this out in a step by step way to be sure we have the right answer.)
   end
@@ -27,6 +28,7 @@ defmodule WiseGPTEx.OpenAIUtils do
       ...> WiseGPTEx.OpenAIUtils.researcher_prompt(message, completions_with_reasoning)
       "What is the capital of France?\n\nOption 1: Paris - This is the correct answer. Paris is the capital of France.\nOption 2: London - This is incorrect. London is the capital of the United Kingdom, not France.\n\nYou are researcher tasked with investigating the answer options provided. List the flaws and faulty logic of each answer option. Let's work this out in a step by step way to be sure we have all the errors\n\n**Answer format:**\nCorrect Answer: <Option Number>\n**If there are multiple correct answers, pick the first one.**"
   """
+  @spec researcher_prompt(binary(), binary()) :: binary()
   def researcher_prompt(message, completions_with_reasoning) do
     ~s(#{message}\n\n#{completions_with_reasoning}\n\nYou are researcher tasked with investigating the answer options provided. List the flaws and faulty logic of each answer option. Let's work this out in a step by step way to be sure we have all the errors\n\n**Answer format:**\nCorrect Answer: <Option Number>\n**If there are multiple correct answers, pick the first one.**)
   end
@@ -53,6 +55,7 @@ defmodule WiseGPTEx.OpenAIUtils do
       "You are a resolver tasked with 1) finding which of the X answer options was best 2) improving that answer, 3) printing the improved answer in full, and 4) adding to the begining of the improved answer special marker <|answerstart|>. Let's work this out in step by step way to be sure we have the right answer:"
 
   """
+  @spec resolver_prompt() :: binary()
   def resolver_prompt do
     ~s{You are a resolver tasked with 1) finding which of the X answer options was best 2) improving that answer, 3) printing the improved answer in full, and 4) adding to the begining of the improved answer special marker <|answerstart|>. Let's work this out in step by step way to be sure we have the right answer:}
   end
@@ -67,6 +70,7 @@ defmodule WiseGPTEx.OpenAIUtils do
       ...> WiseGPTEx.OpenAIUtils.prepare_completion_options(completions)
       "Answer Option 1: PHP\nAnswer Option 2: Elixir\nAnswer Option 3: Go"
   """
+  @spec prepare_completion_options(list(binary())) :: binary()
   def prepare_completion_options(completions) do
     for {completion, i} <- Enum.with_index(completions, 1) do
       ~s(Answer Option #{i}: #{completion})
@@ -85,6 +89,7 @@ defmodule WiseGPTEx.OpenAIUtils do
       ...> WiseGPTEx.OpenAIUtils.trim_resolver_answer(answer)
       "Hello"
   """
+  @spec trim_resolver_answer(binary()) :: binary()
   def trim_resolver_answer(answer) do
     case Regex.run(~r{<\|answerstart\|>(.*)}, answer) do
       [_, answer] -> answer
@@ -121,6 +126,7 @@ defmodule WiseGPTEx.OpenAIUtils do
       ["First, 456 * 23421 = 54,456.", "Second, we get the answer: 10,686,876."]
 
   """
+  @spec extract_completions(map()) :: list(binary())
   def extract_completions(body) do
     for completion <- body["choices"] || [], completion["finish_reason"] == "stop" do
       completion["message"]["content"]
@@ -148,6 +154,7 @@ defmodule WiseGPTEx.OpenAIUtils do
       "Correct Answer: Option 1."
 
   """
+  @spec extract_completion(map()) :: binary()
   def extract_completion(body) do
     choice =
       Enum.find(body["choices"] || [], %{}, fn completion ->
